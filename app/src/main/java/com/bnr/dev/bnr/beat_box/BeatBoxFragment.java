ad62900f20ee6664c3d2bce150af145ec9ced15e
OmarSagazio <omar.sagazio@gmail.com>
2017-05-28 18:47:50 +0200

    Added: BeatBox project

---
+++ app/src/main/java/com/bnr/dev/bnr/beat_box/BeatBoxFragment.java
@@ -0,0 +1,12 @@
+package com.bnr.dev.bnr.beat_box;
+
+/**
+ * Created by dev-w8-12 on 26-May-17.
+ */
+public class BeatBoxFragment {
+
+    // factory
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
